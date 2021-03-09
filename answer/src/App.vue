ab2f5e2dcea9756b68dbc61c17521166ff53c901
sashoyusuke <zyesi731@gmail.com>
2021-03-09 10:15:13 +0900

    first-commit

---
+++ answer/src/App.vue
@@ -0,0 +1,451 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Footer></Footer>
+    <main>
+      <div class="top-mv">
+    <div class="wrap">
+      <p class="top-mv-main-copy">最新のテクノロジーで社会課題を解決</p>
+      <p class="top-mv-sub-copy">Solving social issues with the latest technology</p>
+    </div>
+  </div>
+  <!-- top-mv end -->
+  <main>
+    <!-- top-feature -->
+    <div class="top-feature ptb-m">
+      <div class="top-feature-inner wrap">
+        <h2 class="top-feature-ttl ttl">株式会社TECHの想い</h2>
+        <p class="top-feature-txt">
+          あらゆる産業において、新たなデジタル技術を使ってこれまでにないビジネスモデルを展開する新規参入者が登場し、ゲームチェンジが起ころうとしています。こうした中で、各企業は、競争力維持・強化のために、DXを迅速に進めていくことが求められています。しかし、DXにおいて投資コストは非常にかかります。株式会社TECHでは独自のオペレーションと人材教育により、他社よりも価格を抑えた開発を行います。
+        </p>
+      </div>
+    </div>
+    <!-- top-feature end -->
+    <!-- top-service -->
+    <section class="top-service wrap ptb-m">
+      <h2 class="top-service-ttl ttl">サービス</h2>
+      <div class="top-service-list">
+        <section class="top-service-item">
+          <div class="top-service-item-img">
+            <img src="img/assets/service1.png" alt="IT人材育成事業" />
+          </div>
+          <h3 class="top-service-item-ttl">IT人材育成事業</h3>
+          <p class="top-service-item-description">
+            これまでに培った教育手法を生かして、短期間で即戦力のIT人材を育成します。
+          </p>
+        </section>
+        <section class="top-service-item">
+          <div class="top-service-item-img">
+            <img src="img/assets/service2.png" alt="コーチング事業" />
+          </div>
+          <h3 class="top-service-item-ttl">コーチング事業</h3>
+          <p class="top-service-item-description">
+            目標達成を最短で実現するために、プロのコーチによるマンツーマンのサポートを実施します。
+          </p>
+        </section>
+        <section class="top-service-item">
+          <div class="top-service-item-img">
+            <img src="img/assets/service3.png" alt="ITコンサル事業" />
+          </div>
+          <h3 class="top-service-item-ttl">ITコンサル事業</h3>
+          <p class="top-service-item-description">
+            経営戦略にIT戦略を策定し、システム開発の提案やシステムの最適化を通じて、企業の経営をサポートします。
+          </p>
+        </section>
+        <section class="top-service-item">
+          <div class="top-service-item-img">
+            <img src="img/assets/service4.png" alt="デジタルトランスフォーメーション事業" />
+          </div>
+          <h3 class="top-service-item-ttl">デジタルトランスフォーメーション事業</h3>
+          <p class="top-service-item-description">
+            人々の生活をよりよいものに変革するために様々なデジタル技術を活用し、革新的なソリューションやサービスを生み出します。
+          </p>
+        </section>
+      </div>
+      <a href="service.html" class="top-service-btn btn btn-primary">サービス一覧をみる</a>
+    </section>
+    <!-- top-service end -->
+    <!-- top-news -->
+    <section class="top-news ptb-m">
+      <div class="top-news-inner wrap">
+        <div class="top-news-head">
+          <h2 class="top-news-ttl ttl">ニュース</h2>
+          <a href="news.html" class="top-news-btn btn btn-primary">ニュース一覧をみる</a>
+        </div>
+        <div class="top-news-list">
+          <article class="top-news-item">
+            <a href="#" class="top-news-item-link">
+              <span class="top-news-item-date">2019/07/17</span>
+              <h3 class="top-news-item-ttl">会社を創業しました。</h3>
+            </a>
+          </article>
+          <article class="top-news-item">
+            <a href="#" class="top-news-item-link">
+              <span class="top-news-item-date">2020/04/01</span>
+              <h3 class="top-news-item-ttl">資金調達を行いました。</h3>
+            </a>
+          </article>
+          <article class="top-news-item">
+            <a href="#" class="top-news-item-link">
+              <span class="top-news-item-date">2020/10/01</span>
+              <h3 class="top-news-item-ttl">東京新聞に掲載されました。</h3>
+            </a>
+          </article>
+        </div>
+      </div>
+    </section>
+    <!-- top-news end -->
+    <!-- top-blog -->
+    <section class="top-blog wrap ptb-m">
+      <h2 class="top-blog-ttl ttl">社員ブログ</h2>
+      <div class="top-blog-list">
+        <article class="top-blog-item">
+          <a href="#" class="top-blog-item-thumbnail">
+            <img src="img/assets/blog.png" alt="会社を創業した理由" />
+          </a>
+          <p class="top-blog-item-date">2019/07/17</p>
+          <h3 class="top-blog-item-ttl">
+            <a href="#">会社を創業した理由</a>
+          </h3>
+        </article>
+        <article class="top-blog-item">
+          <a href="#" class="top-blog-item-thumbnail">
+            <img src="img/assets/blog.png" alt="ITエンジニアを育成する最強メソッド" />
+          </a>
+          <div class="top-blog-item-info">
+            <p class="top-blog-item-date">2019/11/11</p>
+          </div>
+          <h3 class="top-blog-item-ttl">
+            <a href="#">ITエンジニアを育成する最強メソッド</a>
+          </h3>
+        </article>
+        <article class="top-blog-item">
+          <a href="#" class="top-blog-item-thumbnail">
+            <img src="img/asstes/blog.png" alt="コーチングによるモチベーション向上理論" />
+          </a>
+          <p class="top-blog-item-date">2020/08/01</p>
+          <h3 class="top-blog-item-ttl">
+            <a href="#">コーチングによるモチベーション向上理論</a>
+          </h3>
+        </article>
+      </div>
+      <a href="blog.html" class="top-blog-btn btn btn-primary">サービス一覧をみる</a>
+    </section>
+    <!-- top-blog end -->
+    <div class="top-company-list ptb-m">
+      <!-- top-company -->
+      <section class="top-company-item">
+        <a href="company.html" class="top-company-link top-company-item-link">
+          会社概要
+        </a>
+      </section>
+      <!-- top-company end -->
+      <!-- top-recruit -->
+      <section class="top-company-item">
+        <a href="recruit.html" class="top-recruit-link top-company-item-link">
+          採用情報
+        </a>
+      </section>
+      <!-- top-recruit end -->
+    </div>
+  </main>
+  <!-- bottom-contact-->
+  <section class="bottom-contact ptb-m">
+    <div class="wrap">
+      <h2 class="bottom-contact-ttl ttl">お問い合わせ</h2>
+      <p class="bottom-contact-txt">
+        お仕事のご依頼や採用に関してはお問い合わせにて承っております。
+      </p>
+      <a href="contact.html" class="bottom-contact-btn btn btn-primary">お問い合わせ</a>
+    </div>
+  </section>
+  </main>
+  </div>
+</template>
+
+<script>
+
+    import Header from "./components/Header.vue";
+    import Footer from "./components/Footer.vue";
+  export default{
+    components:{
+  Header,
+  Footer
+   
+  }
+};
+</script>
+
+<style>
+html, body, div, span, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+abbr, address, cite, code,
+del, dfn, em, img, ins, kbd, q, samp,
+small, strong, sub, sup, var,
+b, i,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section, summary,
+time, mark, audio, video {
+  margin:0;
+  padding:0;
+  border:0;
+  outline:0;
+  font-size:100%;
+  vertical-align:baseline;
+  background:transparent;
+}
+
+body {
+  line-height:1;
+}
+
+article,aside,details,figcaption,figure,
+footer,header,hgroup,menu,nav,section {
+  display:block;
+}
+
+nav ul {
+  list-style:none;
+}
+
+blockquote, q {
+  quotes:none;
+}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content:'';
+  content:none;
+}
+
+a {
+  margin:0;
+  padding:0;
+  font-size:100%;
+  vertical-align:baseline;
+  background:transparent;
+}
+
+/* change colours to suit your needs */
+ins {
+  background-color:#ff9;
+  color:#000;
+  text-decoration:none;
+}
+
+/* change colours to suit your needs */
+mark {
+  background-color:#ff9;
+  color:#000;
+  font-style:italic;
+  font-weight:bold;
+}
+
+del {
+  text-decoration: line-through;
+}
+
+abbr[title], dfn[title] {
+  border-bottom:1px dotted;
+  cursor:help;
+}
+
+table {
+  border-collapse:collapse;
+  border-spacing:0;
+}
+
+/* change border colour to suit your needs */
+hr {
+  display:block;
+  height:1px;
+  border:0;
+  border-top:1px solid #cccccc;
+  margin:1em 0;
+  padding:0;
+}
+
+input, select {
+  vertical-align:middle;
+}
+.top-mv{
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: require("./assets/common/mv.png") center / cover;
+  color: #fff;
+}
+
+.top-mv-main-copy{
+  font-weight: bold;
+  font-size: 50px;
+  text-align: center;
+}
+
+.top-mv-sub-copy{
+  font-weight: bold;
+  font-size: 18px;
+  text-align: center;
+}
+
+.top-feature{
+  color: #fff;
+  background-color: #222;
+}
+
+.top-feature-txt{
+  margin-top: 20px;
+}
+
+.top-service-ttl{
+  text-align: center;
+}
+
+.top-service-list{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.top-service-item{
+  width: 49%;
+  margin-top: 20px;
+  margin-right: calc(100% - (49% * 2));
+}
+
+.top-service-item:nth-child(2n){
+  margin-right: 0;
+}
+
+.top-service-item-ttl{
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.top-service-btn{
+  margin: 40px auto 0;
+}
+
+.top-news{
+  background-color: #222;
+}
+
+.top-news-head{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.top-news-ttl{
+  color: #fff;
+}
+
+.top-news-btn{
+  width: 160px;
+  font-size: 14px;
+  padding: 7px;
+}
+
+.top-news-list{
+  margin-top: 20px;
+}
+
+.top-news-item:not(:first-child){
+  margin-top: 10px;
+}
+
+.top-news-item-link{
+  background-color: #fff;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.top-news-item-date{
+  margin-right: 10px;
+}
+
+.top-blog-ttl{
+  text-align: center;
+}
+
+.top-blog-list{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.top-blog-item{
+  width: 32%;
+  margin-right: calc((100% - (32% * 3)) / 2);
+}
+
+.top-blog-item:nth-child(3n){
+  margin-right: 0;
+}
+
+.top-blog-item-date{
+  margin-top: 5px;
+  margin-right: 20px;
+  font-size: 14px;
+}
+
+.top-blog-item-ttl{
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.top-blog-btn{
+  margin: 40px auto 0;
+}
+
+.top-company-list{
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+  padding-top: 0;
+}
+
+.top-company-item{
+  width: 50%;
+}
+
+.top-company-link{
+  background-color: #cd0224;
+}
+
+.top-recruit-link{
+  background-color: #222;
+}
+
+.top-company-item-link{
+  display: inline-block;
+  width: 100%;
+  line-height: 10;
+  text-align: center;
+}
+
+@media screen and (max-width: 768px){
+  .top-service-item{
+    width: 100%;
+    margin-right: 0;
+  }
+
+  .top-news-item-link{
+    padding: 10px;
+    font-size: 14px;
+  }
+
+  .top-blog-item{
+    width: 100%;
+    margin-right: 0;
+  }
+}
+</style>
---
+++ answer/src/components/Header.vue
@@ -0,0 +1,169 @@
+  <template>
+  <header class="header">
+      <h1 class="header-ttl">
+        <a href="index.html">
+          TECH
+        </a>
+      </h1>
+      <nav class="header-nav">
+        <ul class="header-nav-list">
+          <li class="header-nav-item"><a href="index.html">ホーム</a></li>
+          <li class="header-nav-item"><a href="service.html">事業内容</a></li>
+          <li class="header-nav-item"><a href="news.html">ニュース</a></li>
+          <li class="header-nav-item"><a href="blog.html">社員ブログ</a></li>
+          <li class="header-nav-item"><a href="company.html">会社概要</a></li>
+          <li class="header-nav-item"><a href="recruit.html">採用情報</a></li>
+          <li class="header-nav-item"><a href="contact.html">お問い合わせ</a></li>
+        </ul>
+      </nav>
+      <div class="menu" id="menu" @click="hamburger()" :class="{'active':isclass}">
+        <span class="menu-line-top"></span>
+        <span class="menu-line-middle"></span>
+        <span class="menu-line-bottom"></span>
+      </div>
+      <nav class="drawer-nav" id="drawer-nav" :class="{'open':isClass}">
+        <ul class="drawer-nav-list">
+          <li class="drawer-nav-item"><a href="index.html">ホーム</a></li>
+          <li class="drawer-nav-item"><a href="service.html">事業内容</a></li>
+          <li class="drawer-nav-item"><a href="news.html">ニュース</a></li>
+          <li class="drawer-nav-item"><a href="blog.html">社員ブログ</a></li>
+          <li class="drawer-nav-item"><a href="company.html">会社概要</a></li>
+          <li class="drawer-nav-item"><a href="recruit.html">採用情報</a></li>
+          <li class="drawer-nav-item"><a href="contact.html">お問い合わせ</a></li>
+        </ul>
+      </nav>
+    </header>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      isClass:false
+    };
+  },
+  methods:{
+    hamburger(){
+      this.isClass=!this.isClass;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+  padding: 0 30px;
+  background-color: #fff;
+  position: sticky;
+  left: 0;
+  top: 0;
+  z-index: 1;
+}
+
+.header-ttl {
+  font-size: 24px;
+  width: 80px;
+}
+
+.header-nav-list {
+  display: flex;
+  font-weight: bold;
+}
+
+.header-nav-item:not(:last-child) {
+  margin-right: 30px;
+}
+
+.menu {
+  display: none;
+}
+.drawer-nav {
+  display: none;
+}
+
+@media screen and (max-width:768px){
+   .wrap {
+    width: 85%;
+  }
+
+  .wrap-s {
+    width: 90%;
+  }
+
+  .header-nav {
+    display: none;
+  }
+
+  .menu {
+    display: inline-block;
+    width: 36px;
+    height: 32px;
+    cursor: pointer;
+    position: relative;
+    z-index: 10;
+  }
+
+  .menu-line-top,
+  .menu-line-middle,
+  .menu-line-bottom {
+    display: inline-block;
+    width: 100%;
+    height: 4px;
+    background-color: #000;
+    position: absolute;
+    transition: 0.5s;
+  }
+
+  .menu-line-top {
+    top: 0;
+  }
+
+  .menu-line-middle {
+    top: 14px;
+  }
+
+  .menu-line-bottom {
+    bottom: 0;
+  }
+
+  .menu.open span:nth-of-type(1) {
+    top: 14px;
+    transform: rotate(45deg);
+  }
+
+  .menu.open span:nth-of-type(2) {
+    opacity: 0;
+  }
+
+  .menu.open span:nth-of-type(3) {
+    top: 14px;
+    transform: rotate(-45deg);
+  }
+  .drawer-nav{
+    display: block;
+    position: absolute;
+    height: 100vh;
+    width: 100%;
+    top: 0;
+    left: -100%;
+    background: #fff;
+    transition: .7s;
+  }
+
+  .drawer-nav-list {
+    padding-top: 80px;
+    text-align: center;
+  }
+
+  .drawer-nav-item {
+    margin-top: 40px;
+    color: #0033cc;
+    font-size: 18px;
+  }
+
+}
+</style>
